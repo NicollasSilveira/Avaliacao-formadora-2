4ec6fc57e599a8ba1f653c3263969675bb339513
NicollasSilveira <nicollasprog@gmail.com>
2026-04-20 21:12:47 -0300

    Feat: tarefa concluida

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,10 +1,55 @@
-import { Text, StyleSheet, View } from 'react-native';
+import { useState } from 'react';
+import { StyleSheet, Text, TextInput, View } from 'react-native';
 
 export default function Main() {
-  return (
-    <View>
-          <Text style={styles.text}>Mambo</Text>
+  const [fahrenheit, setFahrenheit] = useState("");
+  const [celsius, setCelsius] = useState("");
 
+  function handleCelsius(value) {
+    setCelsius(value);
+
+    const num = parseFloat(value);
+    if (!isNaN(num)) {
+      const f = (num * 9/5) + 32;
+      setFahrenheit(f.toFixed(2));
+    } else {
+      setFahrenheit("");
+    }
+  }
+
+  function handleFahrenheit(value) {
+    setFahrenheit(value);
+
+    const num = parseFloat(value);
+    if (!isNaN(num)) {
+      const c = (num - 32) * 5/9;
+      setCelsius(c.toFixed(2));
+    } else {
+      setCelsius("");
+    }
+  }
+
+  return (
+    <View style={{ backgroundColor: "#DEA728", paddingBottom: 1920}}>
+      <View style={styles.input_bg}>
+        <Text>Graus Celsius</Text>
+        <TextInput
+          style={styles.input_text}
+          value={celsius}
+          onChangeText={handleCelsius}
+          keyboardType="numeric"
+        />
+      </View>
+
+      <View style={styles.input_bg}>
+        <Text>Graus Fahrenheit</Text>
+        <TextInput
+          style={styles.input_text}
+          value={fahrenheit}
+          onChangeText={handleFahrenheit}
+          keyboardType="numeric"
+        />
+      </View>
     </View>
 
   )
@@ -15,4 +60,18 @@ const styles = StyleSheet.create({
     fontSize: 42,
     color: "#ffffff",
   },
+
+  input_bg : {
+    backgroundColor: "#EFD59A",
+    margin : 50,
+    padding: 40,
+    borderRadius: 16,
+  },
+  input_text : {
+    padding: 12,
+    marginTop: 10,
+    borderColor: "#000",
+    backgroundColor: "#ffffff",
+    borderRadius: 16,
+  }
 });
